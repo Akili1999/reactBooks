4c5293658a6272e76ff2710ea5b57adf6059f6f4
unknown <sluground@gmail.com>
2019-08-03 09:15:04 -0400

    build

---
+++ client/build/precache-manifest.18a1450ac25509dbe55a57abddc11290.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df5acb5b81986d6208765a46e3f8b6a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855b84f186b9358d14d8",
+    "url": "/static/css/main.dff4a9d2.chunk.css"
+  },
+  {
+    "revision": "abcfb3c6b375ea23e3b1",
+    "url": "/static/js/2.98084b2c.chunk.js"
+  },
+  {
+    "revision": "855b84f186b9358d14d8",
+    "url": "/static/js/main.8d0364b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
